f945b2c1910c69024684192da169a9de529a1809
Esteban Mejia <estebandeveloper10@gmail.com>
2023-01-26 14:17:59 -0500

    arreglos de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  dabe: 'practica-app-crud-react-firebase-v1'
+  base: 'practica-app-crud-react-firebase-v1'
 })
